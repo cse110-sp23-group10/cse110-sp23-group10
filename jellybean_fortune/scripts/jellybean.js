c6d50d5409392d106a9eaf698a6503b3b413e8e6
matty-zee <97648068+matty-zee@users.noreply.github.com>
2023-05-23 16:14:17 -0700

    adding jar shaking animation and swapping imgs

--- jellybean_fortune/scripts/jellybean.js
+++ jellybean_fortune/scripts/jellybean.js
@@ -1,5 +1,6 @@
 function shakeJar() {
     var jar = document.getElementById("jar-beans");
+    console.log("shake");
     jar.classList.add("shake");
     setTimeout(function() {
       jar.classList.remove("shake");
