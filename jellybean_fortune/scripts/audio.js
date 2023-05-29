0926322270bbebb5f19fd9b0aedb4cd99c315495
hamesjan <jhan31903@gmail.com>
2023-05-28 18:11:03 -0700

    added jellybean_test.js

--- jellybean_fortune/scripts/audio.js
+++ jellybean_fortune/scripts/audio.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   // toggle mute/unmute audio
@@ -6,12 +6,9 @@ function init() {
   let sound = document.getElementById("sound-icon");
   audio.volume = 0.4;
   let volume = true;
-
-  console.log('init');
-
-  sound.addEventListener('click', function(event) {
-    console.log('mute');
-    
+  console.log("init");
+  sound.addEventListener("click", function (event) {
+    console.log("mute");
     if (volume) {
       audio.volume = 0;
       this.src = "./assets/mute.svg";
