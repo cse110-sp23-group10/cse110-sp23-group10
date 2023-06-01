1ce42410f731eaa8df8431b2d7c044cd0403cdb4
kjchou324 <kjchou@ucsd.edu>
2023-05-31 17:19:11 -0700

    test cases

--- jellybean_fortune/tests/fortune_test.js
+++ jellybean_fortune/tests/fortune_test.js
@@ -16,23 +16,39 @@ const fortuneTest = require("../scripts/fortune.js");
 const delay = (milliseconds) => new Promise((resolve) => setTimeout(resolve, milliseconds));
 
 // Write your tests
+
+test("Clicking on the same jellybean twice should cause the opacity and text to return to default", () => {
+    const beans = document.querySelectorAll('img[alt="Hover over me"]');
+    for(let i = 0; i < beans.length; i ++){
+      beans[i].click();
+      beans[i].click();
+      
+      expect(beans[i].style.opacity).toBe('1')
+      expect(beans[i].style.transform).toBe('scale(1)');
+    }
+  });
+
 test("Clicking on a jellybean should make the others smaller and lower their opacity", () => {
   // Simulate clicking on the beans
+  let styling = true;
+  
   const beans = document.querySelectorAll('img[alt="Hover over me"]');
   for (let i = 0; i < beans.length; i++) {
     beans[i].click();
-    ed
+    delay(500);
+    // check current beans properties
+    styling &&= beans[i].style.opacity == '1';
+    styling &&= beans[i].style.transform == 'scale(1)';
     // check the other beans properties
     for (let j = 0; j < beans.length; j++) {
         if (i === j) continue;
-
+        styling &&= beans[j].style.opacity == '0.5';
+        styling &&= beans[j].style.transform == 'scale(0.8)';
     }
   }
 
   // Assert that the current page URL is the expected value
-  expect(window.location.href).toBe("expected-url");
+  expect(styling).toBe(true);
 });
 
-test("Clickg on the same ", () => {
-  const beans = document.querySelectorAll('img[alt="Hover over me"]');
-});
+
