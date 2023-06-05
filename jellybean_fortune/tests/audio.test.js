a05ac4ed47e9f3738fc3381fbb22aa0fb54fe69d
Austin Li <austinmingli@gmail.com>
2023-06-05 13:30:28 -0700

    add basic audio end to end test

---
+++ jellybean_fortune/tests/audio.test.js
@@ -0,0 +1,22 @@
+describe('audio', () => {
+    beforeAll(async () => {
+        await page.goto(`file://${__dirname}/../jellybean.html`);
+    })
+    it('check sound icon initial state', async () => {
+        const sound = await page.$('#sound-icon');
+        const src = await page.evaluate((sound) => {
+            return sound.src;
+        }, sound)
+        // icon should be unmuted
+        expect(src).toContain('/assets/sound-white.svg');
+    })
+    it('check clicking on sound icon', async () => {
+        const sound = await page.$('#sound-icon');
+        await sound.click();
+        const src = await page.evaluate((sound) => {
+            return sound.src;
+        }, sound)
+        // icon should be muted
+        expect(src).toContain('/assets/mute-white.svg');
+    })
+})
