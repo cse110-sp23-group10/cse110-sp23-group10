3d43659763d0a4f451f446c45d07853ca1285503
mohakvni <mohakvaswani@gmail.com>
2023-06-10 17:36:26 -0700

    Added acconut page and landing page

---
+++ jellybean_fortune/scripts/account.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', init)
+
+/**
+ * Method to initialize and activate the service workers
+ */
+function initializeServiceWorker() {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", async () => {
+        navigator.serviceWorker
+          .register("./sw.js")
+          .then((res) => {
+            console.log(`Service worker loaded`);
+          })
+          .catch((e) => {
+            console.log(`Service Worker failed with error ${e}`);
+          });
+      });
+    } else {
+      console.log("The browser does not support Service Workers");
+    }
+  }
+
+  /**
+   * Function to return to home
+   */
+function GoHome(){
+    window.location.replace("./jellybean.html")
+}
+
+/**
+ * Function that is executed when DOM content is loaded
+ */
+function init() {
+    const name = localStorage.getItem('username')
+    if (name == null){
+        window.location.replace('./landing.html')
+    }
+    const nameEl = document.getElementById('username');
+    nameEl.innerHTML = `Hi ${name}, Welcome to your account page! Here you will find all your past fortunes`;
+    const fortunes = JSON.parse(localStorage.getItem('fortunes'));
+    if (fortunes == null){
+       const no_fortune = document.getElementById('no_fortune');
+       no_fortune.innerHTML = `There are no fortunes to display, go back to the home page to see your fortune`;
+       return ;
+    }
+    const fortune_table = document.getElementById('fortunes');
+    fortune_table.style.display = 'block'
+    for (let index = 0; index < fortunes.length; index++) {
+        const element = fortunes[index];
+        var row = fortune_table.insertRow(index+1);
+        var indx = row.insertCell(0);
+        var jellybean = row.insertCell(1);
+        var fortune = row.insertCell(2);
+        indx.innerHTML = index+1;
+        jellybean.innerHTML = element[0];
+        fortune.innerHTML = element[1];
+    }
+    initializeServiceWorker();
+}
--- jellybean_fortune/scripts/landing.js
+++ jellybean_fortune/scripts/landing.js
@@ -4,9 +4,19 @@ document.addEventListener('DOMContentLoaded', init)
  * Function that is executed when DOM content is loaded
  */
 function init() {
+
+    new Typewriter('#welcome', {
+        strings: ['Welcome to Group-10 Team Jellybean', 'We hope you have a good time'],
+        autoStart: true,
+        loop: true
+      });
     const next_button = document.getElementById('next');
     const name_input = document.getElementById('name');
     next_button.addEventListener('click', () => {
+        if (name_input.value == ""){
+            alert("Please enter a valid name")
+            return
+        }
         localStorage.setItem('username', name_input.value);
         location.replace('./jellybean.html')
     })
